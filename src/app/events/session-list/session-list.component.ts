9359058fcb0fa7853b3f8d60ea6a3f2720cb1cea
Ben Spencer <ben.richard.spencer@outlook.com>
2018-07-29 13:07:01 -0400

    sorting

--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -11,6 +11,8 @@ export class SessionListComponent implements OnChanges {
 
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
+
   visibleSessions: ISession[];
 
   constructor() { }
@@ -18,6 +20,7 @@ export class SessionListComponent implements OnChanges {
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
     }
   }
 
@@ -28,4 +31,17 @@ export class SessionListComponent implements OnChanges {
       this.visibleSessions = this.sessions.filter(x => x.level.toLocaleLowerCase() === filter);
     }
   }
+}
+
+export function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name)
+    return 1;
+  else if (s1.name === s2.name)
+    return 0;
+  else
+    return -1;
+}
+
+export function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
